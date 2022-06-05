8b649bfb14354965aeea849ac297242fb4e73441
Esev <espiritusantiagovictor@gmail.com>
2022-06-05 00:17:04 -0500

    Partition

--- BootConfig.sh
+++ BootConfig.sh
@@ -1,1 +1,16 @@
 #Installation
+Select> Arch Linux Install medium (x86_64, BIOS)
+
+#Map keyboard language
+loadkeys es
+loadkeys us
+
+#Check Internet
+ip link
+ip a s
+ping -c 1 google.com
+
+#Creating Partition
+cfdisk
+>Select: dos # For virtual machine
+
